8843d50976aa357377ef75843a45382b5dc46228
kellyrlarson <krl46@pitt.edu>
2017-02-11 19:07:25 +0000

    added school view

--- webapp/views.py
+++ webapp/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 
 from django.template import Context, Template
 
-from webapp.models import District, School
+from webapp.models import District, School, GradeEnrollment, SportsEnrollment
 
 # Create your views here.
 
@@ -35,3 +35,35 @@ def second_view(request):
 def index(request):
     return render_to_response('christians_test.html')
 
+def school_view(request,schoolid):
+    school = School.objects.filter(composite_id = schoolid)
+    schoolyear="2014-2015"
+    data_ge = GradeEnrollment.objects.filter(school = school, school_year=schoolyear)
+    data_se = SportsEnrollment.objects.filter(school = school, school_year=schoolyear)
+ 
+    for result in data_ge:
+        b = result.boys
+        g = result.girls
+    for result in data_se:
+        b_ath = result.boys
+        g_ath = result.girls
+        t_ath = g_ath + b_ath
+        t = b + g
+        
+    prop_g = g / t * 100
+    prop_fsa = g_ath / t_ath * 100
+    new_needed = g / t * t_ath - g_ath # number of opportunities needed to achieve equity
+    multiplier = prop_g / 5 # i.e. your school is X times more than the legal gap
+    
+    return render_to_response('school_view.html',{
+        "schools":school,
+        "opportunities_needed":new_needed,
+        "cheerleading_num1":5,
+        "cheerleading_num2":7,
+        "state_avg":15,
+        "highsc_avg":19,
+        "boys":b,
+        "girls":g,
+        "boy_ath":b_ath,
+        "girl_ath":g_ath
+    })
