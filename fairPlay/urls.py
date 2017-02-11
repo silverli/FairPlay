e59fa4443d5fb4a04ffbd68838a84cdbe80e08ef
Jamie Klousnitzer <ukigumo121@gmail.com>
2017-02-11 18:00:23 +0000

    static files

--- fairPlay/urls.py
+++ fairPlay/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
+from fairPlay import settings
 import webapp.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^test1$', webapp.views.test_view, name='test_view'),
     url(r'^second_view', webapp.views.second_view, name='second_view'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
