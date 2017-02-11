2393ef51c5f84ad24b25bd8912502d5e403a794e
Christian Thomas <cmt73@pitt.edu>
2017-02-11 16:07:08 +0000

    New stuff!

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from webapp import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    #url(r'^$', views.HomePageView.as_view()),
+    url(r'^', views.index)
 ]
