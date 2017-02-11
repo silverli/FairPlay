ba192a9a787ecfdc5114ce45a6e2cc13c8b072c8
Caitlin Campbell <crc78@pitt.edu>
2017-02-11 20:32:42 +0000

    title IX gap calculation

--- webapp/views.py
+++ webapp/views.py
@@ -4,9 +4,38 @@ from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import Context, Template
-from webapp.models import District, School
+from webapp.models import District, School, GradeEnrollment, SportsEnrollment
 
 # Create your views here.
+def title_nine_calc(school, school_year = None):
+    
+    if(not school_year):
+        school_year = GradeEnrollment.objects.order_by('school_year').last().school_year
+    
+    enrollment = GradeEnrollment.objects.filter(school=school, school_year=school_year)
+    sports_enrollment = SportsEnrollment.objects.filter(school=school, school_year=school_year).exclude(girls=0, boys=0)
+    
+    if(not sports_enrollment):
+        return False
+    
+    total, girls = 0, 0
+    
+    for grade_level in enrollment:
+        total += grade_level.boys + grade_level.girls
+        girls += grade_level.girls
+        
+    percent_girls_enrolled = (float(girls) / float(total))*100
+
+    total_athletes, girl_athletes = 0, 0
+    
+    for sport in sports_enrollment:
+        total_athletes += sport.girls + sport.boys
+        girl_athletes += sport.girls
+
+    girl_athlete_percentage = (float(girl_athletes) / float(total_athletes))*100
+    
+    return percent_girls_enrolled - girl_athlete_percentage
+    
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
@@ -25,26 +54,5 @@ class HomePageView(TemplateView):
             "search":search
         })
     
-    
-    
-        
-# def test_view(request):
-#     query, schools = None, None
-#     print request.GET.get('query')
-#     try:
-#         query = request.GET.get('query')
-#         schools = School.objects.filter(name__istartswith=query)
-#     except:
-#         pass
-
-#     return render_to_response('test_view_1.html',{
-#         "schools":schools,
-#         "query":query
-#     })
-    
-# def second_view(request):
-#     district = request.GET['query']
-
-# def index(request):
-#     return render_to_response('christians_test.html')
-
+def action(request):
+    return render(request, 'action.html', context=None)
